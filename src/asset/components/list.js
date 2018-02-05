22d92cd4df2a9adc509c962b6d11f5aacd513524
0219001784 <0219001784@zte.com.cn>
2018-02-05 18:06:38 +0800

    编辑列表

--- src/asset/components/list.js
+++ src/asset/components/list.js
@@ -1,19 +1,53 @@
 import React,{ Component } from 'react';
-import { Link } from 'react-router-dom'
-class List extends Component {
+import { Link } from 'react-router-dom';
+import {List,Avatar} from 'antd'
+class ListContent extends Component {
     constructor(props){
         super(props);
-        this.state = {}
+        this.state = {
+            dataSource : [
+                {
+                    title:'这个文章标题',
+                    id:'1',
+                    img:'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+                    description:'这是文章描述，这是文章描述，这是文章描述，这是文章描述，这是文章描述，'
+                },
+                {
+                    title:'这个文章标题',
+                    id:'1',
+                    img:'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+                    description:'这是文章描述，这是文章描述，这是文章描述，这是文章描述，这是文章描述，'
+                },
+                {
+                    title:'这个文章标题',
+                    id:'1',
+                    img:'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+                    description:'这是文章描述，这是文章描述，这是文章描述，这是文章描述，这是文章描述，'
+                }
+            ]
+        }
     }
     render(){
         return (
-            <div>
-                <ul>
-                    <li><Link to = "/article/1">跳转到文章详情1</Link></li>
-                    <li><Link to = "/friend/1">跳转到好友详情1</Link></li>
-                </ul>
-            </div>
+            <div className = "list-container">
+                <List
+                    itemLayout="horizontal"
+                    size = "large"
+                    dataSource={this.state.dataSource}
+                    renderItem = {item=>(
+                        <List.Item>
+                            <List.Item.Meta
+                                avatar={<Avatar src={item.img} />}
+                                title={<h3>{item.title}</h3>}
+                                description={item.description}
+                                />
+                        </List.Item>
+                    )}
+                
+                />
+            </div>                
+            
         )
     }
 }
-export default List ;
+export default ListContent ;
--- src/asset/pages/home.js
+++ src/asset/pages/home.js
@@ -1,7 +1,7 @@
 import React,{ Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import Header from '../components/header';
-
+import ListContent from '../components/list';
 class Home extends Component {
     constructor(props){
         super(props);
@@ -10,7 +10,8 @@ class Home extends Component {
     render(){
         return (
             <div className = "container">
-                <Header />                
+                <Header />    
+                <ListContent />            
                 <footer className = "footer">
                     <NavLink to = "/article"><i className = "icon iconfont icon-viewlist"></i><span> 文章列表 </span></NavLink>
                     <NavLink to = "/friend"><i className = "icon iconfont icon-atm"></i><span>好友</span></NavLink>
